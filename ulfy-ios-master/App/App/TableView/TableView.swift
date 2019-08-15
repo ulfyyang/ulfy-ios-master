0a0838a3c6b794fb43b1ff047845ec7c73a3b989
ulfyyang <ulfy920814>
2019-08-15 17:49:17 +0800

    feat: 完成列表页数据源的抽象

--- ulfy-ios-master/App/App/TableView/TableView.swift
+++ ulfy-ios-master/App/App/TableView/TableView.swift
@@ -4,57 +4,44 @@
 //
 
 import UIKit
+import UlfyIOS
 
 // ------------------------------------------------------------------------
 // tableView的基本
 class TableViewController: UIViewController {
-    private var dataSource = DataSource()
-
-    // 注意: Swift中mark注释的格式: MARK:-
-    // MARK:- 属性
-    let cellID = "cell"
+    private var cmList: [TableViewCM] = []
+    private let dataSource = SingleDatasource<TableViewCM>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // 1.创建tableView,并添加的控制器的view
+        dataSource.setOnItemClickListener { view, path, cm in
+            print("点击了\(indexPath.row)")
+        }
+
+        for _ in 1...10 {
+            cmList.append(TableViewCM())
+        }
         let tableView = UITableView(frame: view.bounds)
-
-        // 2.设置数据源代理（因为这里是弱引用，因此必须要在外部以成员变量的方式编写数据源）
-        tableView.dataSource = dataSource
-        tableView.delegate = dataSource
-
-        // 3.添加到控制器的view
-        view.addSubview(tableView)
-
-        // 4.注册cell
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+        dataSource.bindUITableView(tableView: tableView, supportCellType: TableViewCell.self)
+        dataSource.setData(modelList: cmList)
+        self.view.addSubview(tableView)
     }
 }
 
-class DataSource: NSObject, UITableViewDataSource, UITableViewDelegate {
+class TableViewCell: UITableViewCell, IView {
+    private var vm: TableViewCM?
 
-    /// 设置一共有多少组数据
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+    func bind(model: IViewModel) {
+        vm = (model as! TableViewCM)
+        self.textLabel?.text = vm!.content
     }
+}
 
-    /// 设置每组数据的数量
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
-    }
+class TableViewCM: IViewModel {
+    var content: String = "这是测试内容"
 
-    /// 获取具体的行
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
-        // 去除点击效果
-        cell?.selectionStyle = UITableViewCell.SelectionStyle.none
-        cell?.textLabel?.text = "测试数据\(indexPath.row)"
-        return cell!
-    }
-
-    /// 设置点击事件
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("点击了\(indexPath.row)")
+    func getGetViewType() -> UIView.Type {
+        return TableViewCell.self
     }
 }
--- ulfy-ios-master/UlfyIOS/MVVM.swift
+++ ulfy-ios-master/UlfyIOS/MVVM.swift
@@ -16,3 +16,60 @@ public protocol IViewModel {
     /// 数据模型使用的View
     func getGetViewType() -> UIView.Type
 }
+
+/// 针对TableView的数据源适配器
+public class SingleDatasource<M: IViewModel>: NSObject, UITableViewDataSource, UITableViewDelegate {
+    private var modelList:[M]?                                                      // 数据模型列表
+    private var onItemClickListener: ((UITableView, IndexPath, M) -> Void)?         // 单击事件
+
+    /// 构造方法
+    public override init() { }
+
+    /// 构造方法
+    public init(modelList:[M]) {
+        self.modelList = modelList
+    }
+
+    /// 绑定到TabView
+    public func bindUITableView(tableView: UITableView, supportCellType: UITableViewCell.Type...) {
+        for cellType in supportCellType {
+            tableView.register(cellType, forCellReuseIdentifier: NSStringFromClass(cellType))
+        }
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+    /// 设置显示的数据
+    public func setData(modelList: [M]) {
+        self.modelList = modelList
+    }
+
+    /// 设置单击事件
+    public func setOnItemClickListener(onItemClickListener: ((UITableView, IndexPath, M) -> Void)?) {
+        self.onItemClickListener = onItemClickListener
+    }
+
+    /// -----------------------------  复写方法  -----------------------------
+
+    /// 点击事件
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        onItemClickListener?(tableView, indexPath, modelList![indexPath.row])
+    }
+
+    /// 获取每一行的样式
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let convertView = tableView.dequeueReusableCell(withIdentifier: NSStringFromClass(modelList![indexPath.row].getGetViewType()))
+        // 去除点击效果
+        convertView?.selectionStyle = UITableViewCell.SelectionStyle.none
+        // 执行数据绑定
+        if (convertView is IView) {
+            (convertView as! IView).bind(model: modelList![indexPath.row])
+        }
+        return convertView!
+    }
+
+    /// 每组显示的数量
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return modelList?.count ?? 0
+    }
+}
